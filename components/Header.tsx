229a311a3b16d4c5cbe814307a7d7265b5ade6e5
vladnen97 <darkdescent1991@gmail.com>
2023-07-05 01:10:47 +0300

    feat: header comp

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from '@/styles/Header.module.css';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+
+const Header = () => {
+    const router = useRouter()
+
+    return (
+        <header className={s.header}>
+            <nav className={s.nav}>
+                <Link href={'/'} className={router.pathname === '/' ? s.active : null}>Home</Link>
+                <Link href={'/users'} className={router.pathname === '/users' ? s.active : null}>Users</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
