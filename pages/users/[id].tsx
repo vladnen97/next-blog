dd7bc5e07742ec3a2fbde56bf50ffecc3d4176d4
vladnen97 <darkdescent1991@gmail.com>
2023-07-08 22:22:57 +0300

    fix: add posts to current user

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -2,14 +2,16 @@ import MainContainer from '@/components/MainContainer';
 
 export async function getServerSideProps({params}) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const res2 = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const posts = await res2.json()
     const user = await res.json()
     return {
-        props: {user}
+        props: {user, posts: posts.filter(el => el.userId === +params.id)}
     }
 
 }
 
-export default function User({user}) {
+export default function User({user, posts}) {
 
     return (
         <MainContainer title={user.name}>
@@ -17,6 +19,13 @@ export default function User({user}) {
             <div><b>username:</b> {user.username}</div>
             <div><b>email:</b> {user.email}</div>
             <div><b>address:</b> {JSON.stringify(user.address)}</div>
+            <div>posts: </div>
+            <ol>
+                {posts.map(el => <li key={el.id}>
+                    <h5>{el.title}</h5>
+                    <p>{el.body}</p>
+                </li>)}
+            </ol>
         </MainContainer>
     )
 }
