921de7fa610acd4df72ec85e9034840fbd7010e6
vladnen97 <darkdescent1991@gmail.com>
2023-07-08 22:21:11 +0300

    feat: define hostname

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'via.placeholder.com',
+                port: '',
+                pathname: '/150/**'
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
